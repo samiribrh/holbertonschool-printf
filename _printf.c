1f60d92b905783fb02e96e936e8a91ee870d25ba
samiribrh <samiribrahimov2277@gmail.com>
2024-03-28 21:27:17 +0400

    Initial Printf function with c conversion specifier

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int len = 0, i = 0, ispercent, isescape, lenstr;
+	char crntchar, *crntstring;
+
+	va_start(arg, format);
+	while (format[i])
+	{
+		ispercent = format[i] == '%';
+		isescape = format[i] == '\\';
+		if (ispercent && format[i + 1] == 'c')
+		{
+			crntchar = (char)va_arg(arg, int);
+			write(1, &crntchar, 1), i += 2, len++;
+		}
+		else if (ispercent && format[i + 1] == '%')
+		{
+			crntchar = '%';
+			write(1, &crntchar, 1), i += 2, len++;
+		}
+		else if (isescape && format[i + 1] == 'n')
+		{
+			crntchar = '\n';
+			write(1, &crntchar, 1), i += 2, len++;
+		}
+		else if (isescape && format[i + 1] == '\\')
+		{
+			crntchar = '\\';
+			write(1, &crntchar, 1), i += 2, len++;
+		}
+		else
+			write(1, &format[i], 1), i++, len++;
+	}
+    va_end(arg);
+    return len;
+}
