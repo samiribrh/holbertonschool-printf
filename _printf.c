a05d5d94811fd5eceeeb684e129ed7d0e043189a
samiribrh <samiribrahimov2277@gmail.com>
2024-03-28 22:43:05 +0400

    Changed to switch, case

--- _printf.c
+++ _printf.c
@@ -4,15 +4,28 @@
 #include <stdarg.h>
 
 /**
- * _printf - Prints the given format with variables.
- * @format: Main format.
+ * write_char - Writes the given char.
+ * @len: Length of the char in _printf func.
+ * @c: Char to be printed.
  *
- * Return: Number of the chars printed.
+ * Return: void.
+*/
+void write_char(int *len, char c)
+{
+	write(1, &c, 1);
+	(*len)++;
+}
+
+/**
+ * write_str - Writes the given string.
+ * @len: Length of the chars in _printf func.
+ * @str: String to be printed
+ *
+ * Return: void.
  */
 void write_str(int *len, char *str)
 {
-	int lenstr;
-	
+	int lenstr;	
 	if (str == NULL)
 	{
 		write(1, "(null)", 6);
@@ -25,53 +38,44 @@ void write_str(int *len, char *str)
 		*len += lenstr;
 	}
 }
+
+/**
+ * _printf - Prints the given format with variables.
+ * @format: Main format
+ *
+ * Return: Number of the chars printed.
+ */
 int _printf(const char *format, ...)
 {
 	va_list arg;
-	int len = 0, i = 0, ispercent, isescape, lenstr;
+	int len = 0, i = 0;
 	char crntchar, *crntstring;
 
 	va_start(arg, format);
 	while (format[i])
 	{
-		ispercent = format[i] == '%';
-		isescape = format[i] == '\\';
-		if (ispercent && format[i + 1] == 'c')
-		{
-			crntchar = (char)va_arg(arg, int);
-			write(1, &crntchar, 1), i += 2, len++;
-		}
-		else if (ispercent && format[i + 1] == 's')
-		{
-			crntstring = va_arg(arg, char*);
-			if (crntstring == NULL)
-				write(1, "(null)", 6), i += 2, len += 6;
-			else
+		if (format[i] == '%')
+			switch (format[i + 1])
 			{
-				for (lenstr = 0; crntstring[lenstr]; lenstr++)
-					;
-				write(1, crntstring, lenstr), i += 2, len += lenstr;
+				case 'c':
+					crntchar = (char)va_arg(arg, int);
+					write_char(&len, crntchar), i += 2;
+					break;
+				case 's':
+					crntstring = va_arg(arg, char*);
+					write_str(&len, crntstring), i += 2;
+					break;
+				case '%':
+					write_char(&len, '%'), i += 2;
+					break;
+				case '\0':
+					continue;
+				default:
+					write_char(&len, format[i]), i++;
+					break;
 			}
-		}
-		else if (ispercent && format[i + 1] == '%')
-		{
-			crntchar = '%';
-			write(1, &crntchar, 1), i += 2, len++;
-		}
-		else if (ispercent && format[i + 1] == '\0')
-			continue;
-		else if (isescape && format[i + 1] == 'n')
-		{
-			crntchar = '\n';
-			write(1, &crntchar, 1), i += 2, len++;
-		}
-		else if (isescape && format[i + 1] == '\\')
-		{
-			crntchar = '\\';
-			write(1, &crntchar, 1), i += 2, len++;
-		}
 		else
-			write(1, &format[i], 1), i++, len++;
+			write_char(&len, format[i]), i++;
 	}
     va_end(arg);
     return len;
