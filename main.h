27ebe5de62a7162bd926bdff244f3ea453c6fe7d
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-03-30 11:15:56 +0000

    main.h bettystyle

--- main.h
+++ main.h
@@ -5,7 +5,8 @@ int _printf(const char *format, ...);
 void write_char(int *len, char c);
 void write_str(int *len, char *str);
 
-typedef struct typeconverter {
+typedef struct typeconverter
+{
 	char *ltr;
 	void (*func)(int *len, va_list);
 } ltr_func;
