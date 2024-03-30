2233cc1a6a2397e6d89e00e5b55696910b025077
samiribrh <samiribrahimov2277@gmail.com>
2024-03-30 15:00:23 +0400

    Struct for handling functions according to letters

--- main.h
+++ main.h
@@ -5,4 +5,8 @@ int _printf(const char *format, ...);
 void write_char(int *len, char c);
 void write_str(int *len, char *str);
 
+typedef struct typeconverter {
+	char *ltr;
+	void (*func)(int *len, va_list);
+} ltr_func;
 #endif
